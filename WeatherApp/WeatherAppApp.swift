8dfce1686ddd33b3cb8fd403dd9d72375c233787
Deepika Ponnaganti <dponnaganti@cmtelematics.com>
2023-05-01 18:49:30 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Deepika Ponnaganti on 01/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
